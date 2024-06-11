1184df4d1c64a37a0aba6f3d5dca4ca85023480c
angiet04 <164528295+angiet04@users.noreply.github.com>
2024-06-11 10:03:50 -0500

    Add files via upload

---
+++ build/cp311/pywt/version.py
@@ -0,0 +1,11 @@
+# THIS FILE IS GENERATED DURING THE PYWAVELETS BUILD
+# See util/version_utils.py for details
+
+short_version = '1.7.0'
+version = '1.7.0'
+full_version = '1.7.0.dev0+68ed81d'
+git_revision = '68ed81d'
+release = False
+
+if not release:
+    version = full_version
